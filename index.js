d8eb0138b82d790141321bb318e5ed02d3ffcc4c
SkrzypMajster <skrzypmajster@gmail.com>
2019-04-02 06:43:34 +0200

    Add representEnumsAsNumbers function and implement generatorOptions

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const { generateTSFiles } = require("swagger-ts-generator");
 const getSwaggerObject = require("./functions/get-swagger-object");
-// const representEnumsAsNumbers = require("./functions/represent-enums-as-numbers");
 // const collapseContracts = require("./functions/collapse-contract-files");
 
 // Ingore SSL certificate errors
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
-async function generateContracts(swaggerFilePath, contractsDirectory) {
+async function generateContracts(swaggerFilePath, contractsDirectory, options = null) {
     const enumsFileName = "enums";
     const enumsFilePath = `${contractsDirectory}/${enumsFileName}.ts`;
 
@@ -23,6 +22,13 @@ async function generateContracts(swaggerFilePath, contractsDirectory) {
         const swaggerObject = await getSwaggerObject(swaggerFilePath);
 
         generateTSFiles(swaggerObject, generatorOptions);
+
+        if (options && options.representEnumsAsNumbers) {
+            const representEnumsAsNumbers = require("./functions/represent-enums-as-numbers");
+            representEnumsAsNumbers(enumsFilePath).then(() => {
+                console.log("Change enums representation from string to number operation was successful!");
+            });
+        }
     } catch (error) {
         console.error(`[SwaggerGeneratorError]: ${error.message}!`);
     }
